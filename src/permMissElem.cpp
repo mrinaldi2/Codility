d22747625581270b79bd22bbffa0e706b55e48fe
Manuel Rinaldi <manuelrinaldi86@gmail.com>
2020-02-06 21:27:37 +0200

    added cout

--- src/permMissElem.cpp
+++ src/permMissElem.cpp
@@ -7,13 +7,15 @@ int solution(vector<int> &A) {
     int length = A.size();
     int xor_sum = 0;
     for (int i = 0; i < length; i++) {
+        printf("%d ^ %d ^ %d = ", xor_sum, A[i], i+1);
         xor_sum = xor_sum ^ A[i] ^ (i + 1);
+        printf("%d\n", xor_sum);
     }
     return xor_sum^(length + 1);
 }
 
 int main() {
-    vector<int> A = {};
+    vector<int> A = {2,4,6,5,3,7};
     printf("Solutia este %d", solution(A));
     return 0;
 }
